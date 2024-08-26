7d993eca384cf6ecacf8f3ef1f37d67e962a2303
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2024-08-26 11:37:01 -0500

    Used date-fns library to format dates

--- src/components/builder/previousResumes/PreviousResumes.tsx
+++ src/components/builder/previousResumes/PreviousResumes.tsx
@@ -2,6 +2,7 @@
 import { duplicateResume, updateLastAccessed } from '@/lib/slices/resumeSlice';
 import { useAppDispatch, useAppSelector } from '@/lib/store/hooks';
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
+import { format } from 'date-fns';
 import { motion } from 'framer-motion';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
@@ -39,15 +40,24 @@ export default function PreviousResumes() {
             key={resume.id}
             className='h-60 md:h-80 w-full md:w-72 p-3 gap-1 flex flex-col justify-between items-center rounded-md ring-1 ring-light-ring-secondary dark:ring-dark-ring-secondary hover:ring-light-ring-primary hover:dark:ring-dark-ring-primary bg-light-foreground dark:bg-dark-foreground hover:shadow-sm group'
           >
-            <div className='flex-grow flex justify-center items-center'>
-              <ResumeSvg className='text text-5xl md:text-7xl text-light-text-primary dark:text-dark-text-primary group-hover:scale-125 transition duration-300' />
+            <div
+              onClick={() => {
+                dispatch(updateLastAccessed(resume.id));
+                router.push(`/builder/${resume.id}`);
+              }}
+              className='h-60 md:h-80 w-full md:w-72 p-3 gap-1 flex flex-col justify-between items-center cursor-pointer'
+            >
+              <div className='flex-grow flex justify-center items-center'>
+                <ResumeSvg className='text text-5xl md:text-7xl text-light-text-primary dark:text-dark-text-primary group-hover:scale-125 transition duration-300' />
+              </div>
+              <p className='text text-md line-clamp-2 font-bold self-start text-light-text-primary dark:text-dark-text-primary'>
+                {resume.fileName}
+              </p>
+              <p className='text text-sm text-light-text-secondary self-start dark:text-dark-text-secondary'>
+                Last Accessed -{' '}
+                {format(resume.last_accessed, 'HH:mm a eee, dd MMM yyyy')}
+              </p>
             </div>
-            <p className='text text-md line-clamp-2 font-bold self-start text-light-text-primary dark:text-dark-text-primary'>
-              {resume.fileName}
-            </p>
-            <p className='text text-sm text-light-text-secondary self-start dark:text-dark-text-secondary'>
-              Last Accessed - {resume.last_accessed?.slice(0, 16)}
-            </p>
             <div className='mt-2 self-start flex flex-row w-full justify-between items-center'>
               <SecondaryButton
                 onClick={() => {
