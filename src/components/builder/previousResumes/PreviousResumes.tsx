33e85900ea71de810f370879625b816074fee2ac
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2024-08-26 11:47:19 -0500

    refactor: Update PreviousResumes component styles
    Refactor the PreviousResumes component in the builder/previousResumes directory to update the styles and improve the responsiveness. Adjust the height of the resume cards to ensure consistent display across different screen sizes. This update enhances the visual appearance and user experience of the PreviousResumes section.

--- src/components/builder/previousResumes/PreviousResumes.tsx
+++ src/components/builder/previousResumes/PreviousResumes.tsx
@@ -38,14 +38,14 @@ export default function PreviousResumes() {
             viewport={{ once: true }}
             transition={{ type: 'spring', delay: index - index / 1.2 }}
             key={resume.id}
-            className='h-60 md:h-80 w-full md:w-72 p-3 gap-1 flex flex-col justify-between items-center rounded-md ring-1 ring-light-ring-secondary dark:ring-dark-ring-secondary hover:ring-light-ring-primary hover:dark:ring-dark-ring-primary bg-light-foreground dark:bg-dark-foreground hover:shadow-sm group'
+            className='min-h-60 md:min-h-80 w-full md:w-72 p-3 gap-1 flex flex-col justify-between items-center rounded-md ring-1 ring-light-ring-secondary dark:ring-dark-ring-secondary hover:ring-light-ring-primary hover:dark:ring-dark-ring-primary bg-light-foreground dark:bg-dark-foreground hover:shadow-sm group'
           >
             <div
               onClick={() => {
                 dispatch(updateLastAccessed(resume.id));
                 router.push(`/builder/${resume.id}`);
               }}
-              className='h-60 md:h-80 w-full md:w-72 p-3 gap-1 flex flex-col justify-between items-center cursor-pointer'
+              className='min-h-40 md:min-h-60 w-full md:w-72 p-3 gap-1 flex flex-col justify-between items-center cursor-pointer'
             >
               <div className='flex-grow flex justify-center items-center'>
                 <ResumeSvg className='text text-5xl md:text-7xl text-light-text-primary dark:text-dark-text-primary group-hover:scale-125 transition duration-300' />
@@ -55,7 +55,7 @@ export default function PreviousResumes() {
               </p>
               <p className='text text-sm text-light-text-secondary self-start dark:text-dark-text-secondary'>
                 Last Accessed -{' '}
-                {format(resume.last_accessed, 'HH:mm a eee, dd MMM yyyy')}
+                {format(resume.last_accessed, 'HH:mm a eee, MM/dd/yyyy')}
               </p>
             </div>
             <div className='mt-2 self-start flex flex-row w-full justify-between items-center'>
