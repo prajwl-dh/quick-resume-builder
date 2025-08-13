0f4e25e820c263c9c4aeca30c848da0a4ed818b1
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2025-08-13 14:03:18 -0500

    Fixed layout

--- src/components/resume-preview/DesktopResumePreview.tsx
+++ src/components/resume-preview/DesktopResumePreview.tsx
@@ -1,7 +1,6 @@
 import { PDFDownloadLink } from '@react-pdf/renderer';
 import { TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch';
 import { ResumeInterface } from '../builder/previousResumes/ResumeInterface';
-import PrimaryButton from '../buttons/PrimaryButton';
 import SecondaryButton from '../buttons/SecondaryButton';
 import ResumeDocument from './ResumeDocument';
 
@@ -12,30 +11,36 @@ export default function DesktopResumePreview({
 }) {
   return (
     <div className='hidden lg:flex flex-col bg-white dark:bg-dark-foreground justify-center items-center fixed max-h-dvh w-[calc(100vw-(16rem+35vw))] 2xl:w-[calc(100vw-(16rem+40vw))] lg:ml-[calc(16rem+35vw)] 2xl:ml-[calc(16rem+40vw)] overflow-hidden'>
-      <div className='absolute top-0 z-50 w-full h-14 flex justify-end items-center px-4 bg-white dark:bg-dark-foreground'>
-        <div className='flex gap-4'>
-          <SecondaryButton>Reset Zoom</SecondaryButton>
-          <PrimaryButton>
-            <PDFDownloadLink
-              document={<ResumeDocument resume={resume} />}
-              fileName={`Resume_${resume?.profile.fullName}`}
-            >
-              Download PDF
-            </PDFDownloadLink>
-          </PrimaryButton>
-        </div>
-      </div>
-      <TransformWrapper
-        initialScale={0.9}
-        minScale={0.6}
-        maxScale={1}
-        centerZoomedOut
-      >
-        <TransformComponent>
-          <div className='bg-white border-[1px] shadow-md rounded-md mx-10 mb-10 mt-24 px-4 min-h-[90dvh] cursor-grab min-w-[calc(100vw-(16rem+35vw+80px))] 2xl:min-w-[calc(100vw-(16rem+40vw+80px))] antialiased'>
-            <ResumeDocument resume={resume} />
-          </div>
-        </TransformComponent>
+      <TransformWrapper initialScale={1} minScale={0.6} maxScale={1}>
+        {({ setTransform }) => (
+          <>
+            <div className='absolute top-0 z-50 w-full h-14 flex justify-end items-center px-4 bg-white dark:bg-dark-foreground'>
+              <div className='flex gap-4'>
+                <SecondaryButton
+                  onClick={() => {
+                    setTransform(0, 0, 1);
+                  }}
+                >
+                  Reset Zoom
+                </SecondaryButton>
+
+                <PDFDownloadLink
+                  className='text-light-button-primary-text dark:text-dark-button-primary-text bg-light-button-primary-background dark:bg-dark-button-primary-background hover:bg-light-button-primary-active dark:hover:bg-dark-button-primary-active ring-1 ring-light-button-primary-background dark:ring-dark-button-primary-background hover:ring-light-button-primary-active dark:hover:ring-dark-button-primary-active px-3 py-1.5 rounded-md shadow-sm outline-none transition duration-200 text-md hover:-translate-y-0.5'
+                  document={<ResumeDocument resume={resume} />}
+                  fileName={`Resume_${resume?.profile.fullName}`}
+                >
+                  Download PDF
+                </PDFDownloadLink>
+              </div>
+            </div>
+
+            <TransformComponent>
+              <div className='bg-white border-[1px] shadow-md rounded-md mx-10 mb-10 mt-24 px-4 min-h-[90dvh] cursor-grab min-w-[calc(100vw-(16rem+35vw+80px))] 2xl:min-w-[calc(100vw-(16rem+40vw+80px))] antialiased'>
+                <ResumeDocument resume={resume} />
+              </div>
+            </TransformComponent>
+          </>
+        )}
       </TransformWrapper>
     </div>
   );
--- src/components/resume-preview/MobileResumePreview.tsx
+++ src/components/resume-preview/MobileResumePreview.tsx
@@ -1,7 +1,6 @@
 import { PDFDownloadLink } from '@react-pdf/renderer';
 import { TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch';
 import { ResumeInterface } from '../builder/previousResumes/ResumeInterface';
-import PrimaryButton from '../buttons/PrimaryButton';
 import SecondaryButton from '../buttons/SecondaryButton';
 import ResumeDocument from './ResumeDocument';
 
@@ -12,31 +11,41 @@ export default function MobileResumePreview({
 }) {
   return (
     <div className='bg-light-background dark:bg-dark-foreground h-[calc(100dvh-56px)] lg:hidden w-full fixed mt-14 flex flex-col justify-center items-center overflow-hidden'>
-      <div className='absolute top-0 z-50 w-full h-14 flex justify-end items-center px-4 bg-light-background dark:bg-dark-foreground'>
-        <div className='flex gap-4'>
-          <SecondaryButton>Reset Zoom</SecondaryButton>
-          <PrimaryButton>
-            <PDFDownloadLink
-              document={<ResumeDocument resume={resume} />}
-              fileName={`Resume_${resume?.profile.fullName}`}
-            >
-              Download PDF
-            </PDFDownloadLink>
-          </PrimaryButton>
-        </div>
-      </div>
       <TransformWrapper
-        initialScale={0.9}
+        initialScale={1}
         minScale={0.6}
         maxScale={1}
-        centerOnInit
         centerZoomedOut
       >
-        <TransformComponent>
-          <div className='bg-white border-[1px] shadow-md rounded-md mx-10 mb-10 px-4 mt-24 min-h-[90dvh] w-dvw cursor-grab'>
-            <ResumeDocument resume={resume} />
-          </div>
-        </TransformComponent>
+        {({ setTransform }) => (
+          <>
+            <div className='absolute top-0 z-50 w-full h-14 flex justify-end items-center px-4 bg-light-background dark:bg-dark-foreground'>
+              <div className='flex gap-4'>
+                <SecondaryButton
+                  onClick={() => {
+                    setTransform(0, 0, 1);
+                  }}
+                >
+                  Reset Zoom
+                </SecondaryButton>
+
+                <PDFDownloadLink
+                  className='text-light-button-primary-text dark:text-dark-button-primary-text bg-light-button-primary-background dark:bg-dark-button-primary-background hover:bg-light-button-primary-active dark:hover:bg-dark-button-primary-active ring-1 ring-light-button-primary-background dark:ring-dark-button-primary-background hover:ring-light-button-primary-active dark:hover:ring-dark-button-primary-active px-3 py-1.5 rounded-md shadow-sm outline-none transition duration-200 text-md hover:-translate-y-0.5'
+                  document={<ResumeDocument resume={resume} />}
+                  fileName={`Resume_${resume?.profile.fullName}`}
+                >
+                  Download PDF
+                </PDFDownloadLink>
+              </div>
+            </div>
+
+            <TransformComponent>
+              <div className='bg-white border-[1px] shadow-md rounded-md mx-2 mb-10 px-4 mt-24 min-h-[90dvh] cursor-grab'>
+                <ResumeDocument resume={resume} />
+              </div>
+            </TransformComponent>
+          </>
+        )}
       </TransformWrapper>
     </div>
   );
