5bfedf1c5622cb131b59f0b4ceeb35d97c9e1acd
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2025-08-13 14:44:51 -0500

    Updated site metadata on layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,10 +7,27 @@ import { Providers } from './provider';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://get-quick-resume.netlify.app'),
   title:
     'Quick Resume - Build Free Professional ATS Friendly Resumes Instantly',
   description:
     "Quick Resume is your go-to platform for creating professional ats friendly resumes effortlessly. No login/signup required, save your resumes locally, and generate PDFs with ease. Perfect for job seekers looking to make a great impression. And it's completely free!",
+  openGraph: {
+    url: 'https://get-quick-resume.netlify.app',
+    type: 'website',
+    title:
+      'Quick Resume - Build Free Professional ATS Friendly Resumes Instantly',
+    description:
+      "Quick Resume is your go-to platform for creating professional ats friendly resumes effortlessly. No login/signup required, save your resumes locally, and generate PDFs with ease. Perfect for job seekers looking to make a great impression. And it's completely free!",
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: 'get-quick-resume.netlify.app',
+    title:
+      'Quick Resume - Build Free Professional ATS Friendly Resumes Instantly',
+    description:
+      "Quick Resume is your go-to platform for creating professional ats friendly resumes effortlessly. No login/signup required, save your resumes locally, and generate PDFs with ease. Perfect for job seekers looking to make a great impression. And it's completely free!",
+  },
 };
 
 export default function RootLayout({
